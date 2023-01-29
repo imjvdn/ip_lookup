0ed4e5c0077ab79d16f7ff4e35a1f952d9ba8028
Jadan Morrow <jadanmorrow@outlook.com>
2023-01-29 10:43:04 -0700

    Update formatting

--- iplookup.ps1
+++ iplookup.ps1
@@ -6,14 +6,25 @@ if (!([System.Net.IPAddress]::TryParse($ipAddress, [ref]$null))) {
 $url = "https://ipinfo.io/$ipAddress/json"
 try {
     $location = (Invoke-WebRequest -Uri $url -ErrorAction Stop).Content | ConvertFrom-Json
-    Write-Host "ISP: $($location.org)"
-    Write-Host "City: $($location.city)"
-    Write-Host "Region: $($location.region)"
-    Write-Host "Country: $($location.country)"
-    Write-Host "Latitude: $($location.loc.Split(",")[0])"
-    Write-Host "Longitude: $($location.loc.Split(",")[1])"
+    Write-Host "------- IP Info Lookup --------" -ForegroundColor Green
+    Write-Host "IP Address:" $ipAddress -ForegroundColor Green
+Write-Host "Hostname: $($location.hostname)" -ForegroundColor Green
+Write-Host "ISP: $($location.org)" -ForegroundColor Green
+Write-Host "City: $($location.city)" -ForegroundColor Green
+Write-Host "Region: $($location.region)" -ForegroundColor Green
+Write-Host "Country: $($location.country)" -ForegroundColor Green
+Write-Host "Latitude: $($location.loc.Split(",")[0])" -ForegroundColor Green
+Write-Host "Longitude: $($location.loc.Split(",")[1])"-ForegroundColor Green
+Write-Host "Timezone: $($location.timezone)"-ForegroundColor Green
+    Write-Host "-------------------------------"-ForegroundColor Green
+    Write-Host ""
+    Write-Host "--------- System Time ---------"-ForegroundColor Green
+    Write-Host "    $(Get-Date -Format "dd MMMM yyyy HH:mm:ss")"-ForegroundColor Green
+    Write-Host "-------------------------------"-ForegroundColor Green
+
+    
 } catch {
-    Write-Host "Error getting location information for IP address $ipAddress"
+    Write-Host "Error getting location information for IP address $ipAddress" 
 }
 
  
