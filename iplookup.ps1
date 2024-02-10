cd73e233919e1028523f6d83e877259d2e41122c
Jadan Morrow <jadanmorrow@outlook.com>
2024-02-09 17:47:15 -0800

    cleaning up

--- iplookup.ps1
+++ iplookup.ps1
@@ -8,14 +8,14 @@ try {
     $location = (Invoke-WebRequest -Uri $url -ErrorAction Stop).Content | ConvertFrom-Json
     Write-Host "------- IP Info Lookup --------" -ForegroundColor Cyan
     Write-Host "IP Address:" $ipAddress -ForegroundColor Cyan
-Write-Host "Hostname: $($location.hostname)" -ForegroundColor Cyan
-Write-Host "ISP: $($location.org)" -ForegroundColor Cyan
-Write-Host "City: $($location.city)" -ForegroundColor Cyan
-Write-Host "Region: $($location.region)" -ForegroundColor Cyan
-Write-Host "Country: $($location.country)" -ForegroundColor Cyan
-Write-Host "Latitude: $($location.loc.Split(",")[0])" -ForegroundColor Cyan
-Write-Host "Longitude: $($location.loc.Split(",")[1])"-ForegroundColor Cyan
-Write-Host "Timezone: $($location.timezone)"-ForegroundColor Cyan
+    Write-Host "Hostname: $($location.hostname)" -ForegroundColor Cyan
+    Write-Host "ISP: $($location.org)" -ForegroundColor Cyan
+    Write-Host "City: $($location.city)" -ForegroundColor Cyan
+    Write-Host "Region: $($location.region)" -ForegroundColor Cyan
+    Write-Host "Country: $($location.country)" -ForegroundColor Cyan
+    Write-Host "Latitude: $($location.loc.Split(",")[0])" -ForegroundColor Cyan
+    Write-Host "Longitude: $($location.loc.Split(",")[1])"-ForegroundColor Cyan
+    Write-Host "Timezone: $($location.timezone)"-ForegroundColor Cyan
     Write-Host "-------------------------------"-ForegroundColor Cyan
     Write-Host "--------- System Time ---------"-ForegroundColor Cyan
     Write-Host "    $(Get-Date -Format "dd MMMM yyyy HH:mm:ss")"-ForegroundColor Cyan
